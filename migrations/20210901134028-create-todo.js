7b1194a98dc0ba933b1fa52fa21c8338a2e16134
steven4program <stevenchang421@gmail.com>
2021-09-01 22:09:51 +0800

    add default seeder

--- migrations/20210901134028-create-todo.js
+++ migrations/20210901134028-create-todo.js
@@ -14,7 +14,7 @@ module.exports = {
       },
       isDone: {
         type: Sequelize.BOOLEAN,
-        allowNull: false
+        defaultValue: false
       },
       createdAt: {
         allowNull: false,
